658d091db4283acf0b99f8c0e62c989355576a87
Dump-Duck <93809062+Dump-Duck@users.noreply.github.com>
2024-05-06 22:57:06 +0700

    Application Permission Update

--- E-LearningApplication/E-LearningApplication/Home.cs
+++ E-LearningApplication/E-LearningApplication/Home.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,48 @@ namespace E_LearningApplication
 {
     public partial class Home : Form
     {
-        public Home()
+        private int currentUserID;
+        string stringConnect = @"Data Source=DESKTOP-NC6U1Q4\MSSQL_SERVER;Initial Catalog=E-LearningApplicationDB;Integrated Security=True;Encrypt=False";
+        string sql;
+        SqlConnection sqlConnection;
+        SqlCommand sqlCommand;
+
+        private bool hasPermission(int userID, string role)
+        {
+            using (sqlConnection = new SqlConnection(stringConnect))
+            {
+                sqlConnection.Open();
+                sql = @"SELECT COUNT(*) FROM Users WHERE Username = @UserID AND Role = @Role";
+                using(sqlCommand = new SqlCommand(sql, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@UserID", userID);
+                    sqlCommand.Parameters.AddWithValue("@Role", role);
+                    int count = (int)sqlCommand.ExecuteScalar();
+                    return count > 0;
+                }
+            }
+        }
+
+        private void checkPermission(int userID)
+        {
+            if(!hasPermission(userID, "Admin"))
+            {
+                usersManagement.Visible = false;
+            }
+            if(!hasPermission(userID, "Teacher"))
+            {
+                coursesManagement.Visible = false;
+            }
+            if(!hasPermission(userID, "Student"))
+            {
+                myCourses.Visible = false;
+            }
+        }
+        public Home(int userID)
         {
             InitializeComponent();
+            currentUserID = userID;
+            checkPermission(currentUserID);
         }
 
         private void coursesToolStripMenuItem_Click(object sender, EventArgs e)
